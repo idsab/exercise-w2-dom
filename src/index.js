3b0c0160f3f84b23a93152bbefbb9b31b766c8fc
idsab <idasabic@gmail.com>
2024-02-12 20:06:13 +0100

    failed attempt to change color

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("Start Here 😉");
+
+
+const headerBox = document.getElementsByClassName("box")
+  const darkTheme = document.getElementById("dark")
+  const lightTheme = document.getElementById("light")
+  const crazyTheme = document.getElementById("crazy")
+  
+
+  //tried changing box color to black didnt work//
+  
+  //darkTheme.onclick = () => { 
+   // headerBox.style.backgroundColor = "black"; 
+//
+
